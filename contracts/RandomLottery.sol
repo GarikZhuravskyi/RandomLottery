02e074bf827d0b98d4c2bcb304115b8288da2d98
Zhuravskyi Heorhii Viktorovich <j_g_v@rambler.ru>
2022-02-21 22:35:03 +0200

    Rewrote the algorithm due to some calculation mistakes.

--- contracts/RandomLottery.sol
+++ contracts/RandomLottery.sol
@@ -5,15 +5,19 @@ import "hardhat/console.sol";
 
 contract RandomLottery{
 
-    // Параметры лотереи
+    // Params of the lottery
     address payable owner;
     address payable winner;
     uint reward = address(this).balance;
     uint token;
+    uint tokens;
+    uint time;
 
-    constructor(address payable ownerAddress, uint tokenCost){
-        owner = ownerAddress;
+    constructor (uint tokenCost, uint lotteryTime, uint tokenAmount){
+        owner = payable(msg.sender);
         token = tokenCost;
+        time = lotteryTime + block.timestamp;
+        tokens = tokenAmount;
     }
 
     address[] public players;
@@ -22,33 +26,51 @@ contract RandomLottery{
     /// Switch to true when the lottery ends
     bool ended;
 
+    // Declaring an event for an appropriate ending
     event LotteryEnded(address winner, uint amount);
 
     function buyTicket() external payable {
 
+        // Next two lines should check if a user can afford the lottery ticket and the lottery is still going.
         require(msg.sender.balance >= token, "Sorry, you can not afford the ticket");
-        require(reward + msg.value <= 1000 ether, "Sorry, maximum amount of ether is exceeded");
+        require(tokens > 0 && time > 0, "Sorry, the lottery is over");
         reward += msg.value;
+        tokens -= 1;
 
-        if(playerBalances[msg.sender] == 0){
+        // If a user is already participating in the lottery, this means he's buying additional ticket.
+        // In this case, we're gonna add the new ticket price to already collected.
+        // Thus player can buy as many tickets as he wants to increase his own chance of winning.
+        if(playerBalances[msg.sender] == 0)
             players.push(msg.sender);
-            playerBalances[msg.sender] += msg.value;
-        }
+        
+        playerBalances[msg.sender] += msg.value;
     }
 
-    // Using a pseudorandom to determine a winner among the players
+    // Using a pseudorandom to determine a winner among the player
     function DetermineWinner() private{
+        /// We must add 1 to make this work, 'cause it goes from 0 to N in programming, but we need an AMOUNT.
         uint random = uint(keccak256(abi.encodePacked(block.timestamp, msg.sender))) % reward + 1;
-        winner = payable(players[random]);
+        unchecked{
+            for(uint i=0; i<players.length; i++){
+                if(playerBalances[players[i]]>=random){
+                    winner = payable(players[i]);
+                    break;
+                }
+                random -= playerBalances[players[i]];
+            }
+        }
     }
 
     function EndLottery() external{
 
+        require(payable(msg.sender) == owner);
+
         ended = true;
 
         uint playerReward = reward/10 * 9;
-
         uint fee = reward - playerReward;
+
+        /// Publicly declaring the winner and his prize.
         emit LotteryEnded(winner, playerReward);
 
         winner.transfer(playerReward);
--- test/sample-test.js
+++ test/sample-test.js
@@ -2,7 +2,25 @@ const { expect } = require("chai");
 const { ethers } = require("hardhat");
 
 describe("RandomLottery", function () {
-  it("Should return the new greeting once it's changed", async function () {
-    const Greeter = await ethers.getContractFactory("RandomLottery");
-  });
+  let owner;
+  let addr1;
+  let addr2;
+  let RandomLottery;
+  let token;
+  let tokens;
+  let time;
+  let addrss = [];
+  let hardhat;
+
+  beforeEach(async function(){
+    RandomLottery = await ethers.getContractFactory("RandomLottery");
+    [owner, addr1, addr2, ...addrss] = await ethers.getSigners();
+    hardhat = await RandomLottery.deploy(); 
+  })
+
+  describe("Deployment", function(){
+    it("Should set the right owner", async function(){
+      expect(await hardhat.owner()).to.equal(owner.address);
+    })
+  })
 });
